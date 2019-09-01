6a3a6d64562b2e24e6cfd3f304c7fc10aa9592ca
桐铭 梁 <werltm@hotmail.com>
2019-09-01 15:54:58 +0800

    添加项目文件结构

---
+++ DotnetconfChina/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetconfChina.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
