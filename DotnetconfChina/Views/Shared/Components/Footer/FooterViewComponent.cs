7266a6eda3927054030e05666cd9a8a1c5eae0f9
桐铭 梁 <werltm@hotmail.com>
2019-10-12 14:53:06 +0800

    优化内容，新增合作伙伴以及底部栏目图。 修改banner图的显示。 隐藏视频章鱼。

---
+++ DotnetconfChina/Views/Shared/Components/Footer/FooterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetconfChina.Views.Shared.Components.Footer
+{
+    public class FooterViewComponent : ViewComponent
+    {
+
+
+
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+
+    }
+}
