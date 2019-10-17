df699b8918fa217f3a0eb6d4152c58097ea17e52
David Zheng <zhy391884651@live.com>
2019-10-17 23:50:40 +0800

    Add 出品人以及格式

--- DotnetconfChina/Views/Shared/Components/Ticket/TicketViewComponent.cs
+++ DotnetconfChina/Views/Shared/Components/Ticket/TicketViewComponent.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DotnetconfChina.Views.Shared.Components.Heroback
+namespace DotnetconfChina.Views.Shared.Components.Ticket
 {
     public class TicketViewComponent : ViewComponent
     {
