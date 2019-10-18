76bfc5f2559c3e16d3be46888d723c2172a0e3a9
葛斌 <gbvaq@foxmail.com>
2019-10-18 16:16:43 +0800

    提交一次，剩余演讲嘉宾卡片排版

---
+++ DotnetconfChina/Views/Shared/Components/Producer/ProducerViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetconfChina.Views.Shared.Components.Producer
+{
+    public class ProducerViewComponent : ViewComponent
+    {
+
+
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+
+    }
+}
