50bf0df76c83e5f9d21864e180d5065b33dc88ca
梁桐铭 <werltm@hotmail.com>
2019-11-06 20:47:42 +0800

    添加讲师信息。调整部分组件的bug

---
+++ DotnetconfChina/Views/Shared/Components/Schedule/ScheduleViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetconfChina.Views.Shared.Components.Schedule
+{
+    public class ScheduleViewComponent : ViewComponent
+    {
+
+
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+
+    }
+}
